4f68235f66577f46b1bd2a26736b6959cc593b32
Munna Ahmed <munnaahmed2025@gmail.com>
2024-04-27 21:04:41 +0600

    feature: horizontal top scrollbar

---
+++ dist/js/scrollbar.js
@@ -0,0 +1,24 @@
+// scroll bar as horizontal progress bar
+
+// console.log(
+//   document.body.scrollHeight, // full height of the website
+//   window.innerHeight, // height of the screen
+//   // pageYOffset,
+//   scrollY
+// );
+
+// pageYOffset is aliar for scrollY
+
+let value = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener("resize", () => {
+  value = document.body.scrollHeight - window.innerHeight;
+  // console.log(value);
+});
+
+window.addEventListener("scroll", () => {
+  const scroll = scrollY;
+  const progress = Math.round((scroll / value) * 100);
+  // console.log(scroll, progress);
+  document.querySelector(".scroll-progress-bar").style.width = `${progress}vw`;
+});
