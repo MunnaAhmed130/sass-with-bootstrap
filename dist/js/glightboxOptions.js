a0f43d318344ed7701d601ee9bc09b08a0704829
Munna Ahmed <munnaahmed2025@gmail.com>
2024-04-29 00:23:22 +0600

    move glightbox options to an external file

---
+++ dist/js/glightboxOptions.js
@@ -0,0 +1,12 @@
+const lightbox = GLightbox({
+  // href: "https://www.youtube.com/watch?v=Ga6RYejo6Hk",
+  // type: "video",
+  // source: "youtube", //vimeo, youtube or local
+  // width: 900,
+  href: "assets/images/arts/intro-section-illustration.png",
+  type: "image",
+  autoplayVideos: true,
+  zoomable: true,
+  // title: "My Title",
+  // description: "Example",
+});
